136c51656de31134fb1027e7c715a1ce2f467899
Harsh <harshrathi.dev@gmail.com>
2024-01-15 11:06:08 +0530

    set ReceiptToken to 18 decimal

--- packages/smartcontracts/src/contracts/ReceiptToken.sol
+++ packages/smartcontracts/src/contracts/ReceiptToken.sol
@@ -7,10 +7,6 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 contract ReceiptToken is ERC20, Ownable {
   constructor(string memory name, string memory symbol) ERC20(name, symbol) {
   }
-
-  function decimals() public view virtual override returns (uint8) {
-    return 8;
-  }
   
   function mint(address account, uint256 amount) external onlyOwner {
     _mint(account, amount);
