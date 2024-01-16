ca840e49f1c7fd4c1864a789819883222099d06b
Harsh <harshrathi.dev@gmail.com>
2024-01-16 15:48:45 +0530

    fixed contract deployment

--- packages/smartcontracts/src/scripts/deployStakingLsdProxy.ts
+++ packages/smartcontracts/src/scripts/deployStakingLsdProxy.ts
@@ -1,33 +1,49 @@
 import { ethers } from 'hardhat';
 
-import { StakingLsdProxy, StakingLsdV1__factory } from '../generated';
+import { StakingLsdProxy, StakingLsdV1, StakingLsdV1__factory } from '../generated';
 import { verify } from './utils/verify';
 
-// npx hardhat run --network sepolia ./scripts/deployStakingLsdProxy.ts
 export async function deployStakingLsdProxy({
   adminAddress,
   walletAddress,
   stakingLsdV1Address
 }: InputsForInitialization): Promise<StakingLsdProxy> {
   const contract = await ethers.getContractFactory('StakingLsdProxy');
+  const receiptTokenName = 'DFI STAKING RECEIPT TOKEN'
+  const receiptTokenSymbol = 'xDFI'
   const encodedData = StakingLsdV1__factory.createInterface().encodeFunctionData('initialize', [
     // admin address, or timelock contract address
     adminAddress,
     // withdraw address
     walletAddress,
     // receipt token name
-    'DFI STAKING RECEIPT TOKEN',
+    receiptTokenName,
     // receipt token symbol
-    'xDFI'
+    receiptTokenSymbol
   ]);
   const stakingLsdProxy = await contract.deploy(stakingLsdV1Address, encodedData);
   await stakingLsdProxy.waitForDeployment();
-  const stakingLsdProxyAddress = await stakingLsdProxy.getAddress()
+  const stakingLsdProxyAddress = "0xCce7B4FE99F7fa97DE6e031886D77B43447E81Ae" //await stakingLsdProxy.getAddress()
   const txn = await stakingLsdProxy.deploymentTransaction()
   await txn?.wait(10);
   console.log('Proxy Address: ', stakingLsdProxyAddress);
   console.log('Verifying...');
-  await verify({ contractAddress: stakingLsdProxyAddress, args: [stakingLsdV1Address, encodedData], contract: "./src/contracts/StakingLsdProxy.sol:StakingLsdProxy" });
+  await verify({
+    contractAddress: stakingLsdProxyAddress, 
+    args: [stakingLsdV1Address, encodedData], 
+    contract: "contracts/StakingLsdProxy.sol:StakingLsdProxy" 
+  });
+  // verify receipt token 
+  const StakingLsdUpgradeable = await ethers.getContractFactory('StakingLsdV1');
+  const proxyStakingLsd = StakingLsdUpgradeable.attach(stakingLsdProxyAddress) as StakingLsdV1;
+
+  const receiptToken = await proxyStakingLsd.receiptToken()
+  console.log('Receipt Token Address: ', receiptToken);
+  console.log('Verifying...');
+  await verify({ 
+    contractAddress: receiptToken, 
+    args: [receiptTokenName, receiptTokenSymbol], 
+  });
   return stakingLsdProxy;
 }
 
