70324463ac493e7babce3b21069ac1dfd806a007
nattadex <elocinnat99@gmail.com>
2024-01-30 16:31:19 +0800

    updated the stylings and max width

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,7 +4,7 @@ import UnlockPower from "@/app/ui/sections/UnlockPower";
 import HowItWorksSection from "@/app/ui/sections/HowItWorksSection";
 export default function Page() {
   return (
-    <div className="flex justify-center items-center w-full my-12 md:my-24">
+    <div className="flex flex-col justify-center items-center w-full my-12 md:my-24">
       <UnlockPower />
       <HowItWorksSection />
     </div>
