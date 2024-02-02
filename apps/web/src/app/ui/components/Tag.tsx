63d2b47ad07219a1839e647686c9cb9976c32fe4
chloe <44501120+chloezxyy@users.noreply.github.com>
2024-02-02 11:47:42 +0800

    fix(ui-ux): text sizes

--- apps/web/src/app/ui/components/Tag.tsx
+++ apps/web/src/app/ui/components/Tag.tsx
@@ -1,6 +1,6 @@
 export default function Tag() {
   return (
-    <div className="border rounded-5 bg-light-00 w-fit py-1 px-4 rounded-[20px]">
+    <div className="border rounded-5 bg-light-00 py-2 px-4 rounded-[20px] flex items-center justify-center">
       <span className="text-[10px] text-light-1000 font-bold leading-3 tracking-[.5px] uppercase">
         Beta release
       </span>
