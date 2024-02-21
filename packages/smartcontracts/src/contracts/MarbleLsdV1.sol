dbc0218abac74716cb1d433bf81d44abce6e3e5c
Harsh R <53080940+fullstackninja864@users.noreply.github.com>
2024-02-21 09:34:03 +0530

    Update packages/smartcontracts/src/contracts/MarbleLsdV1.sol
    Co-authored-by: Pierre Gee <pierre.gabaldon@gmail.com>

--- packages/smartcontracts/src/contracts/MarbleLsdV1.sol
+++ packages/smartcontracts/src/contracts/MarbleLsdV1.sol
@@ -70,7 +70,7 @@ contract MarbleLsdV1 is UUPSUpgradeable, EIP712Upgradeable, AccessControlUpgrade
 
   /**
    * @notice Emitted when deposit/mint happen on smart contract
-   * @param sender Address initiating deposit/mint
+   * @param sender address initiating the exchange of assets, owned by owner, for shares
    * @param owner Address receiving shares
    * @param assets Amount of asset that being staked
    * @param shares Amount of shares that being alloted
