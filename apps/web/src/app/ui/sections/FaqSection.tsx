52f46c36a19adce0b85ca4ac3f22bfe2580aeb95
Harsh <harshrathi.dev@gmail.com>
2024-02-28 16:57:30 +0530

    added todo

--- apps/web/src/app/ui/sections/FaqSection.tsx
+++ apps/web/src/app/ui/sections/FaqSection.tsx
@@ -5,6 +5,7 @@ import { Disclosure, Transition } from "@headlessui/react";
 import { FiChevronDown } from "react-icons/fi";
 import { Tag } from "@/app/ui/components/Tag";
 
+// TODO (Harsh) update content
 const FaqItems = [
   {
     testId: "what-is-liquid-staking",
