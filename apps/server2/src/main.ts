be95fd583670df701c7b1a64eda310c31bf9faa8
nattadex <elocinnat99@gmail.com>
2024-03-05 13:31:17 +0800

    attempting to push server changes

---
+++ apps/server2/src/main.ts
@@ -0,0 +1,9 @@
+import { AppModule } from "./AppModule";
+import {MetachainLsdServerApp} from "./MetachainLsdServerApp";
+
+async function bootstrap() {
+  const app = new MetachainLsdServerApp(AppModule);
+  await app.start();
+}
+
+void bootstrap();
