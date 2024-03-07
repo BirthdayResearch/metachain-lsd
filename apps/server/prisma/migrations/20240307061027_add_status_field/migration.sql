a016270a05f4b80e05ba73545bd88251ef9ff3af
nattadex <elocinnat99@gmail.com>
2024-03-07 15:27:10 +0800

    removed duplication prisma migration

--- apps/server/prisma/migrations/20240307061027_add_status_field/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "SubscriptionStatus" AS ENUM ('ACTIVE', 'INACTIVE');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "status" "SubscriptionStatus" NOT NULL DEFAULT 'ACTIVE';
