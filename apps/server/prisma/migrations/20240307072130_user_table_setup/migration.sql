a2499b407d4aa9a9a12b44a5289fcb1407a5e764
nattadex <elocinnat99@gmail.com>
2024-03-07 15:21:56 +0800

    update schema to include status

--- apps/server/prisma/migrations/20240307072130_user_table_setup/migration.sql
+++ apps/server/prisma/migrations/20240307072130_user_table_setup/migration.sql
@@ -1,7 +1,11 @@
+-- CreateEnum
+CREATE TYPE "SubscriptionStatus" AS ENUM ('ACTIVE', 'INACTIVE');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
+    "status" "SubscriptionStatus" NOT NULL DEFAULT 'ACTIVE',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
