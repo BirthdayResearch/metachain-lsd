cbdb45fc65c422fa5d34aca3a979c777511b3486
nattadex <elocinnat99@gmail.com>
2024-03-19 15:46:33 +0800

    stored server url in env file

--- apps/web/src/app/store/marbleFiApi.ts
+++ apps/web/src/app/store/marbleFiApi.ts
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { CreateUserI } from "@/app/types/user";
+import process from "process";
 
 // eslint-disable-next-line import/prefer-default-export
 export const marbleFiApi = createApi({
   reducerPath: "marbleFiApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5741" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_SERVER_URL }),
   endpoints: (builder) => ({
     createUser: builder.mutation<any, CreateUserI>({
       query: ({ email, status }) => ({
