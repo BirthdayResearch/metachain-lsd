01d1d81c29787c83de8bd97b9ec23139e7b5dc9d
pierregee <pierre@cakedefi.com>
2024-03-22 13:14:06 +0800

    fix alignment

--- apps/web/src/app/ui/components/Header.tsx
+++ apps/web/src/app/ui/components/Header.tsx
@@ -29,7 +29,7 @@ export default function Header({
           "sticky z-50 py-5 lg:flex top-0 header-bg md:py-10 w-full flex items-center justify-center font-mono text-sm mb-8 md:mb-16",
         )}
       >
-        <div className="flex w-full items-center px-5 min-w-fit max-w-5xl justify-between">
+        <div className="flex w-full items-center px-5 min-w-fit max-w-5xl justify-between md:pt-0 pt-2">
           <MarbleFiLogo customStyle="md:w-full w-[132px] h-[30px] sm:h-auto" />
 
           {/* Mobile Header View */}
