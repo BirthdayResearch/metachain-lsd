f518f205b92115f0ad909ab82b0984bda3c93853
chloe <44501120+chloezxyy@users.noreply.github.com>
2024-04-01 15:47:13 +0800

    chore(server): /health check endpoint

---
+++ apps/server/src/health/PrismaHealthController.ts
@@ -0,0 +1,19 @@
+import { Controller, Get } from "@nestjs/common";
+import { HealthCheck, HealthCheckService } from "@nestjs/terminus";
+import { SkipThrottle } from "@nestjs/throttler";
+import { PrismaHealthIndicator } from "./PrismaHealth";
+
+@Controller("health")
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private prisma: PrismaHealthIndicator,
+  ) {}
+
+  @SkipThrottle()
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([() => this.prisma.isHealthy("database")]);
+  }
+}
