59008bc80e6be8b4f21498fa15a617eb2d1ee96d
pierregee <pierre@cakedefi.com>
2024-04-03 11:22:31 +0800

    fix(ui): focus on wallet integration

--- apps/web/src/app/stake/page.tsx
+++ apps/web/src/app/stake/page.tsx
@@ -13,14 +13,12 @@ import { useEffect, useMemo, useState } from "react";
 import { parseEther } from "viem";
 import { ConnectKitButton } from "connectkit";
 import BigNumber from "bignumber.js";
-import { InputCard } from "@/app/ui/components/InputCard";
 import { CTAButton } from "@/app/ui/components/button/CTAButton";
 import { useRouter } from "next/navigation";
 import { useDfiPrice } from "@/app/lib/hooks/useDfiPrice";
 import { useContractContext } from "@/app/lib/context/ContractContext";
 import { useNetworkContext } from "@waveshq/walletkit-ui";
 import DialogueBox from "@/app/stake/components/DialogueBox";
-import Statistics from "@/app/stake/components/Statistics";
 
 export default function Stake() {
   const { push } = useRouter();
@@ -84,36 +82,6 @@ export default function Stake() {
     hash: depositFundData?.hash,
   });
 
-  async function submitStake() {
-    if (isDepositTxnSuccess) {
-      // redirect to main page
-      push("/");
-      return;
-    }
-    // additional checks to ensure that the user's wallet balance is sufficient to cover the deposit amount
-    // ensure that the entered amount meets the min. deposit req defined by the contract's minDeposit Variable
-
-    if (isDepositPaused) {
-      // TODO display popup
-      console.log("Deposit is paused");
-      // display message to user that deposit is paused
-      return;
-    }
-    console.log("call stake");
-    try {
-      if (isDepositPaused) {
-        // TODO display popups
-        console.log("Deposit is paused");
-        // display message to user that deposit is paused
-        return;
-      }
-      writeDepositTxn?.();
-    } catch (error) {
-      console.error("Deposit failed:", error);
-      // Handle error (e.g., display error message to user)
-    }
-  }
-
   function getActionBtnLabel() {
     switch (true) {
       case isDepositTxnSuccess:
@@ -135,70 +103,21 @@ export default function Stake() {
   return (
     <DialogueBox>
       <div className="grid gap-y-10">
-        <div>
-          <h3 className="text-2xl font-semibold mb-8">Stake DFI</h3>
-          <div className="flex w-full justify-between mb-1">
-            <span className="text-xs font-medium">Enter amount to stake</span>
-            {isWalletConnected ? (
-              <p>
-                <span className="opacity-40">Available: </span>
-                <span className="font-semibold opacity-70">
-                  {walletBalanceAmount}
-                </span>
-              </p>
-            ) : (
-              <span className="text-xs text-warning font-semibold">
-                Connect wallet to get started
-              </span>
+        <div className="gap-y-6 grid">
+          <ConnectKitButton.Custom>
+            {({ show }) => (
+              <CTAButton
+                testID="instant-transfer-btn"
+                label={getActionBtnLabel()}
+                customStyle="w-full md:py-5"
+                isLoading={isDepositInProgress || isDepositTxnInProgress}
+                disabled={isDepositInProgress || isDepositTxnInProgress}
+                onClick={!isConnected ? show : () => submitStake()}
+              />
             )}
-          </div>
-          <div className="gap-y-6 grid">
-            <InputCard
-              amount={stakeAmount}
-              onChange={setStakeAmount}
-              maxAmount={new BigNumber(walletBalanceAmount)}
-              value={stakedValue}
-              displayPercentageBtn={isWalletConnected}
-              disabled={isDepositInProgress || isDepositTxnInProgress}
-            />
-            <section>
-              <TransactionRow
-                label="You will receive"
-                value={`${previewDepositFormatted} mDFI`}
-              />
-              <TransactionRow label="Exchange rate" value="1 mDFI = 1 DFI" />
-              {/* TODO fee schedule*/}
-              <TransactionRow
-                label="Estimated transaction cost"
-                value="$0.00"
-              />
-            </section>
-            <ConnectKitButton.Custom>
-              {({ show }) => (
-                <CTAButton
-                  testID="instant-transfer-btn"
-                  label={getActionBtnLabel()}
-                  customStyle="w-full md:py-5"
-                  isLoading={isDepositInProgress || isDepositTxnInProgress}
-                  disabled={isDepositInProgress || isDepositTxnInProgress}
-                  onClick={!isConnected ? show : () => submitStake()}
-                />
-              )}
-            </ConnectKitButton.Custom>
-          </div>
+          </ConnectKitButton.Custom>
         </div>
-
-        <Statistics />
       </div>
     </DialogueBox>
-  );
-}
-
-function TransactionRow({ label, value }: { label: string; value: string }) {
-  return (
-    <div className="flex flex-row justify-between py-[10px]">
-      <span>{label}</span>
-      <span className="text-sm font-semibold">{value}</span>
-    </div>
   );
 }
