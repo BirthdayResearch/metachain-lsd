f38a6799b56c59440e91cb95abb7d566b4a6e264
chloe <44501120+chloezxyy@users.noreply.github.com>
2024-04-22 11:19:33 +0800

    fix: update test id app footer

--- apps/web/src/app/app/components/AppNavigationBarMobile.tsx
+++ apps/web/src/app/app/components/AppNavigationBarMobile.tsx
@@ -16,7 +16,10 @@ export default function AppNavigationBarMobile({ onClose }: { onClose: any }) {
       }}
     >
       <div className="flex items-center md:px-12 px-5 py-8 min-w-fit max-w-5xl justify-between">
-        <MarbleFiLogo customStyle="md:w-full w-[132px] h-[30px] sm:h-auto" />
+        <MarbleFiLogo
+          customStyle="md:w-full w-[132px] h-[30px] sm:h-auto"
+          testId="app-footer-mobile"
+        />
         <IoMdClose
           size={30}
           onClick={onClose}
