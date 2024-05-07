9a48ed831f05dbef108a492a8466a6334f356dcd
chloe <44501120+chloezxyy@users.noreply.github.com>
2024-05-07 14:47:05 +0800

    chore: code refactor for stake page

--- apps/web/src/app/app/stake/page.tsx
+++ apps/web/src/app/app/stake/page.tsx
@@ -43,65 +43,69 @@ export default function Stake() {
   }
 
   return (
-    <Panel>
-      <div className="w-full gap-y-5">
-        <h3 className="text-2xl font-semibold">Stake DFI</h3>
-        <div className="flex flex-col w-full justify-between gap-y-5 mb-12 md:mb-10 lg:mb-16">
-          <div className="mt-10">
-            <div className="mb-5">
-              <div className="flex justify-between gap-y-2 mb-2">
-                <span className="text-xs md:text-sm py-1">
-                  How much do you want to stake?
-                </span>
+    <div>
+      <Panel>
+        <div className="w-full gap-y-5">
+          <h3 className="text-2xl font-semibold">Stake DFI</h3>
+          <div className="flex flex-col w-full justify-between gap-y-5 mb-12 md:mb-10 lg:mb-16">
+            <div className="mt-10">
+              <div className="mb-5">
+                <div className="flex justify-between gap-y-2 mb-2">
+                  <span className="text-xs md:text-sm py-1">
+                    How much do you want to stake?
+                  </span>
+                  <WalletDetails
+                    isWalletConnected={isConnected}
+                    style="md:block hidden"
+                  />
+                </div>
+                <div className="pb-2 md:pb-0">
+                  <InputCard
+                    maxAmount={maxStakeAmount}
+                    value={stakeAmount}
+                    setAmount={setStakeAmount}
+                    usdAmount={(new BigNumber(stakeAmount).isNaN()
+                      ? new BigNumber(0)
+                      : new BigNumber(stakeAmount)
+                    ).toFixed(2)} // TODO use USDT price to calculate DFI amount
+                    onChange={(value) => setStakeAmount(value)}
+                  />
+                </div>
                 <WalletDetails
                   isWalletConnected={isConnected}
-                  style="md:block hidden"
+                  style="block md:hidden"
                 />
               </div>
-              <div className="pb-2 md:pb-0">
-                <InputCard
-                  maxAmount={maxStakeAmount}
-                  value={stakeAmount}
-                  setAmount={setStakeAmount}
-                  usdAmount={(new BigNumber(stakeAmount).isNaN()
-                    ? new BigNumber(0)
-                    : new BigNumber(stakeAmount)
-                  ).toFixed(2)} // TODO use USDT price to calculate DFI amount
-                  onChange={(value) => setStakeAmount(value)}
+              <div className="grid gap-y-2">
+                <span className="text-xs md:text-sm py-1">
+                  Receiving address
+                </span>
+                <AddressInput
+                  value={receivingWalletAddress}
+                  setValue={setReceivingWalletAddress}
+                  placeholder="Connect a wallet"
+                  isDisabled={!isConnected}
                 />
               </div>
-              <WalletDetails
-                isWalletConnected={isConnected}
-                style="block md:hidden"
-              />
             </div>
-            <div className="grid gap-y-2">
-              <span className="text-xs md:text-sm py-1">Receiving address</span>
-              <AddressInput
-                value={receivingWalletAddress}
-                setValue={setReceivingWalletAddress}
-                placeholder="Connect a wallet"
-                isDisabled={!isConnected}
-              />
+            <div>
+              <TransactionRow label="You will receive" value="0.00 mDFI" />
+              <TransactionRow label="Exchange rate" value="1 mDFI = 1 DFI" />
+              <TransactionRow label="Max transaction cost" value="$0.00" />
             </div>
           </div>
-          <div>
-            <TransactionRow label="You will receive" value="0.00 mDFI" />
-            <TransactionRow label="Exchange rate" value="1 mDFI = 1 DFI" />
-            <TransactionRow label="Max transaction cost" value="$0.00" />
-          </div>
+          <ConnectKitButton.Custom>
+            {({ show }) => (
+              <CTAButton
+                testID="instant-transfer-btn"
+                label={getActionBtnLabel()}
+                customStyle="w-full md:py-5"
+                onClick={!isConnected ? show : () => submitStake()}
+              />
+            )}
+          </ConnectKitButton.Custom>
         </div>
-        <ConnectKitButton.Custom>
-          {({ show }) => (
-            <CTAButton
-              testID="instant-transfer-btn"
-              label={getActionBtnLabel()}
-              customStyle="w-full md:py-5"
-              onClick={!isConnected ? show : () => submitStake()}
-            />
-          )}
-        </ConnectKitButton.Custom>
-      </div>
-    </Panel>
+      </Panel>
+    </div>
   );
 }
