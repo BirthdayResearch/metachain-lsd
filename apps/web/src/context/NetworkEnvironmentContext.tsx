2cc9af31e507e52bdcd890d4380245aa6a6fa81a
Harsh R <53080940+fullstackninja864@users.noreply.github.com>
2024-05-07 12:21:43 +0530

    Update NetworkEnvironmentContext.tsx

--- apps/web/src/context/NetworkEnvironmentContext.tsx
+++ apps/web/src/context/NetworkEnvironmentContext.tsx
@@ -63,7 +63,7 @@ export function NetworkEnvironmentProvider({
   // TODO @chloezxyyy url routing on different network
   const updateRoute = (value: EnvironmentNetwork) => {
     if (value !== defaultNetwork) {
-      router.push(pathName + "?" + getQueryStaring("network", value));
+      router.replace(pathName + "?" + getQueryStaring("network", value));
     }
   };
 
