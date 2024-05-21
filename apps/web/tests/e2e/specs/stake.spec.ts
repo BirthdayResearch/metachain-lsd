059c40ac12b6672eeb1c20b7f5729f472a0eb50e
Harsh <harshrathi.dev@gmail.com>
2024-05-21 10:38:07 +0530

    added formating

--- apps/web/tests/e2e/specs/stake.spec.ts
+++ apps/web/tests/e2e/specs/stake.spec.ts
@@ -1,18 +1,16 @@
 describe("Connects with Metamask", () => {
   before(() => {
     // Attach up wallet
-    // cy.setupMetamask(Cypress.env('privateKey'), Cypress.env('network'));
+    cy.setupMetamask(Cypress.env("privateKey"), Cypress.env("network"));
     // Logout all
-    // cy.disconnectMetamaskWalletFromAllDapps();
+    cy.disconnectMetamaskWalletFromAllDapps();
     // Visit wallet connect page
+    cy.visit("http://localhost:3000/app/stake");
   });
 
   it("synpress should show the stake page", () => {
     // Wait until the page is loaded
-    cy.visit("http://localhost:3000/app/stake");
     cy.contains("Connect wallet to get started").should("exist");
-    // cy.findByTestId("connect-button").should("exist").click();
-
     cy.findByTestId("cta-button-instant-transfer-btn").click();
     cy.contains("MetaMask").should("exist").click();
 
