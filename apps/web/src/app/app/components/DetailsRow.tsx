cf6b74712d661fbf7784e5eae5be02a9ff719337
Harsh <harshrathi.dev@gmail.com>
2024-05-22 10:12:37 +0530

    fixed DetailsRow link

--- apps/web/src/app/app/components/DetailsRow.tsx
+++ apps/web/src/app/app/components/DetailsRow.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { FiCopy, FiExternalLink } from "react-icons/fi";
 import { useContractContext } from "@/context/ContractContext";
+import Link from "next/link";
 
 export default function DetailsRow({
   label,
@@ -36,14 +37,14 @@ export default function DetailsRow({
           >
             <FiCopy size={16} />
           </button>
-          <button
+          <Link
             className="hover:bg-light-1000/[0.05] active:bg-light-100/[0.7] rounded-[20px] p-2 cursor-pointer flex flex-row"
-            onClick={() => {
-              window.open(`${ExplorerURL}/${linkType}/${value}`);
-            }}
+            href={`${ExplorerURL}/${linkType}/${value}`}
+            rel="noopener noreferrer"
+            target="_blank"
           >
             <FiExternalLink size={16} />
-          </button>
+          </Link>
         </div>
       </div>
     </div>
