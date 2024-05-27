f9fabaadf8d9cf33d0983eb3e2073d697b7a293d
nattadex <elocinnat99@gmail.com>
2024-05-27 17:10:42 +0800

    integrated the is withdrawals paused page

--- apps/web/src/app/app/withdraw/page.tsx
+++ apps/web/src/app/app/withdraw/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import { useAccount, useBalance } from "wagmi";
-import React, { useEffect, useState } from "react";
+import { useAccount, useBalance, useReadContract } from "wagmi";
+import React, { useEffect, useMemo, useState } from "react";
 import { ConnectKitButton } from "connectkit";
 import { CTAButton } from "@/components/button/CTAButton";
 import Panel from "@/app/app/stake/components/Panel";
@@ -14,9 +14,12 @@ import { formatEther } from "ethers";
 import { useGetReadContractConfigs } from "@/hooks/useGetReadContractConfigs";
 import { NumericTransactionRow } from "@/app/app/components/NumericTransactionRow";
 import { getDecimalPlace } from "@/lib/textHelper";
+import { useContractContext } from "@/context/ContractContext";
+import PausedWithdrawalsPage from "@/app/app/withdraw/components/PausedWithdrawalsPage";
 
 export default function Withdraw() {
   const { address, isConnected, status, chainId } = useAccount();
+  const { MarbleLsdProxy } = useContractContext();
 
   const { data: walletBalance } = useBalance({
     address,
@@ -31,6 +34,18 @@ export default function Withdraw() {
 
   const balance = formatEther(walletBalance?.value.toString() ?? "0");
 
+  const { data: isWithdrawalPausedData } = useReadContract({
+    address: MarbleLsdProxy.address,
+    abi: MarbleLsdProxy.abi,
+    functionName: "isWithdrawalPaused",
+    query: {
+      enabled: isConnected,
+    },
+  });
+  const isWithdrawalPaused = useMemo(() => {
+    return (isWithdrawalPausedData as boolean) ?? false;
+  }, [isWithdrawalPausedData]);
+
   useEffect(() => {
     setWalletBalanceAmount(balance); // set wallet balance
   }, [address, status, walletBalance]);
@@ -38,88 +53,92 @@ export default function Withdraw() {
   return (
     <Panel>
       <div>
-        <div className="w-full gap-y-5">
-          <h3 className="text-2xl font-semibold">Withdraw DFI</h3>
-          <div className="flex flex-col w-full justify-between gap-y-5">
-            <div className="mt-10 md:mt-7 lg:mt-10">
-              <div className="mb-5">
-                <div className="flex items-center justify-between gap-y-2 mb-2">
-                  <span className="text-xs md:text-sm py-1">
-                    How much do you want to withdraw?
-                  </span>
+        {!isWithdrawalPaused ? (
+          <div className="w-full gap-y-5">
+            <h3 className="text-2xl font-semibold">Withdraw DFI</h3>
+            <div className="flex flex-col w-full justify-between gap-y-5">
+              <div className="mt-10 md:mt-7 lg:mt-10">
+                <div className="mb-5">
+                  <div className="flex items-center justify-between gap-y-2 mb-2">
+                    <span className="text-xs md:text-sm py-1">
+                      How much do you want to withdraw?
+                    </span>
+                    <WalletDetails
+                      walletBalanceAmount={walletBalanceAmount}
+                      isWalletConnected={isConnected}
+                      style="md:block hidden"
+                    />
+                  </div>
+                </div>
+                <div className="grid gap-y-2">
+                  <InputCard
+                    isConnected={isConnected}
+                    maxAmount={new BigNumber(walletBalanceAmount)}
+                    minAmount={new BigNumber(minDepositAmount)}
+                    error={amountError}
+                    setError={setAmountError}
+                    value={withdrawAmount}
+                    setAmount={setWithdrawAmount}
+                  >
+                    <Image
+                      data-testid="mdfi-icon"
+                      src="/icons/mdfi-icon.svg"
+                      alt="MDFI icon"
+                      className="min-w-6"
+                      priority
+                      width={24}
+                      height={24}
+                    />
+                  </InputCard>
                   <WalletDetails
                     walletBalanceAmount={walletBalanceAmount}
                     isWalletConnected={isConnected}
-                    style="md:block hidden"
+                    style="block md:hidden"
                   />
                 </div>
               </div>
-              <div className="grid gap-y-2">
-                <InputCard
-                  isConnected={isConnected}
-                  maxAmount={new BigNumber(walletBalanceAmount)}
-                  minAmount={new BigNumber(minDepositAmount)}
-                  error={amountError}
-                  setError={setAmountError}
-                  value={withdrawAmount}
-                  setAmount={setWithdrawAmount}
-                >
-                  <Image
-                    data-testid="mdfi-icon"
-                    src="/icons/mdfi-icon.svg"
-                    alt="MDFI icon"
-                    className="min-w-6"
-                    priority
-                    width={24}
-                    height={24}
-                  />
-                </InputCard>
-                <WalletDetails
-                  walletBalanceAmount={walletBalanceAmount}
-                  isWalletConnected={isConnected}
-                  style="block md:hidden"
+              <div className="flex flex-col gap-y-1 mb-10 md:mb-7 lg:mb-10">
+                <NumericTransactionRow
+                  label="You will receive"
+                  value={{
+                    value: 0.0,
+                    decimalScale: getDecimalPlace(0.0),
+                    suffix: ` mDFI`,
+                  }}
+                />
+                <NumericTransactionRow
+                  label="Exchange rate"
+                  value={{
+                    value: 1,
+                    suffix: " DFI",
+                    decimalScale: getDecimalPlace(1),
+                    prefix: `1 mDFI = `,
+                    trimTrailingZeros: false,
+                  }}
+                />
+                <NumericTransactionRow
+                  label="Max transaction cost"
+                  value={{
+                    value: 3.23,
+                    decimalScale: getDecimalPlace(3.23),
+                    prefix: "$",
+                  }}
                 />
               </div>
             </div>
-            <div className="flex flex-col gap-y-1 mb-10 md:mb-7 lg:mb-10">
-              <NumericTransactionRow
-                label="You will receive"
-                value={{
-                  value: 0.0,
-                  decimalScale: getDecimalPlace(0.0),
-                  suffix: ` mDFI`,
-                }}
-              />
-              <NumericTransactionRow
-                label="Exchange rate"
-                value={{
-                  value: 1,
-                  suffix: " DFI",
-                  decimalScale: getDecimalPlace(1),
-                  prefix: `1 mDFI = `,
-                  trimTrailingZeros: false,
-                }}
-              />
-              <NumericTransactionRow
-                label="Max transaction cost"
-                value={{
-                  value: 3.23,
-                  decimalScale: getDecimalPlace(3.23),
-                  prefix: "$",
-                }}
-              />
-            </div>
+            <ConnectKitButton.Custom>
+              {({ show }) => (
+                <CTAButton
+                  testId="instant-transfer-btn"
+                  label={isConnected ? "Withdraw mDFI" : "Connect wallet"}
+                  customStyle="w-full md:py-5"
+                />
+              )}
+            </ConnectKitButton.Custom>
           </div>
-          <ConnectKitButton.Custom>
-            {({ show }) => (
-              <CTAButton
-                testId="instant-transfer-btn"
-                label={isConnected ? "Withdraw mDFI" : "Connect wallet"}
-                customStyle="w-full md:py-5"
-              />
-            )}
-          </ConnectKitButton.Custom>
-        </div>
+        ) : (
+          <PausedWithdrawalsPage />
+        )}
         <ComplimentarySection />
       </div>
     </Panel>
