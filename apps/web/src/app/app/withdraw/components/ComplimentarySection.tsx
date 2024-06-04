7f452550b9efa56c25971a581af2feacb9675609
nattadex <elocinnat99@gmail.com>
2024-06-04 13:50:24 +0800

    changed to button-bg-gradient-1

--- apps/web/src/app/app/withdraw/components/ComplimentarySection.tsx
+++ apps/web/src/app/app/withdraw/components/ComplimentarySection.tsx
@@ -28,7 +28,7 @@ export default function ComplimentarySection({
           testId="faq-button-complimentary-section"
           customStyle="!px-3 !py-3 md:!py-2"
           customTextStyle="whitespace-nowrap text-xs font-medium"
-          customBgColor="withdraw-button-bg"
+          customBgColor="button-bg-gradient-1"
         />
       </div>
     </div>
