2647f2e9c2a971dca63ca6447a08d490bb412e8a
nattadex <elocinnat99@gmail.com>
2024-06-04 16:03:18 +0800

    HoverPopover.tsx comments

--- apps/web/src/app/app/components/HoverPopover.tsx
+++ apps/web/src/app/app/components/HoverPopover.tsx
@@ -1,20 +1,22 @@
-import { PropsWithChildren, ReactNode, useState } from "react";
+import { ReactElement, ReactNode, useState } from "react";
 import { Placement, useFloating, shift } from "@floating-ui/react-dom";
 import clsx from "clsx";
 
-interface IconPopoverProps {
+export function HoverPopover({
+  popover,
+  placement,
+  className,
+  children,
+}: {
   popover: string | ReactNode;
   placement?: Placement;
   className?: string;
-}
-
-export function HoverPopover(
-  props: PropsWithChildren<IconPopoverProps>,
-): JSX.Element {
+  children?: ReactElement;
+}): JSX.Element {
   const [isHover, setIsHover] = useState(false);
 
   const { x, y, reference, floating, strategy } = useFloating({
-    placement: props.placement ?? "bottom",
+    placement: placement ?? "bottom",
     middleware: [shift()],
     strategy: "fixed",
   });
@@ -26,36 +28,29 @@ export function HoverPopover(
         onMouseOver={() => setIsHover(true)}
         onMouseLeave={() => setIsHover(false)}
         onTouchCancel={() => setIsHover(false)}
-        className={clsx(props.className)}
+        className={clsx(className)}
       >
-        {props.children}
+        {children}
       </div>
-
-      {(() => {
-        if (!isHover) {
-          return null;
-        }
-
-        return (
-          <div
-            ref={floating}
-            style={{
-              position: strategy,
-              top: y ?? "",
-              left: x ?? "",
-            }}
-            className="p-2 z-20"
-          >
-            {typeof props.popover === "string" ? (
-              <div className="rounded-[10px] bg-dark-00 px-4 py-3 text-dark-1000 text-xs text-left shadow-md max-w-xs">
-                {props.popover}
-              </div>
-            ) : (
-              props.popover
-            )}
-          </div>
-        );
-      })()}
+      {isHover ? (
+        <div
+          ref={floating}
+          style={{
+            position: strategy,
+            top: y ?? "",
+            left: x ?? "",
+          }}
+          className="p-2 z-20"
+        >
+          {typeof popover === "string" ? (
+            <div className="rounded-[10px] bg-dark-00 px-4 py-3 text-dark-1000 text-xs text-left shadow-md max-w-xs">
+              {popover}
+            </div>
+          ) : (
+            popover
+          )}
+        </div>
+      ) : null}
     </>
   );
 }
