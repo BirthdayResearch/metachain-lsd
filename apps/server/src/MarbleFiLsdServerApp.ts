067e1b933ffdc2519ce558471bea2503ba95bfe0
Harsh <harshrathi.dev@gmail.com>
2024-06-07 15:02:58 +0530

    chore

--- apps/server/src/MarbleFiLsdServerApp.ts
+++ apps/server/src/MarbleFiLsdServerApp.ts
@@ -80,7 +80,7 @@ export class MarbleFiLsdServerApp<
     const app = await this.init();
 
     const PORT = process.env.PORT || 5741;
-    await app.listen(PORT, "0.0.0.0").then(() => {
+    await app.listen(PORT, "::").then(() => {
       // eslint-disable-next-line no-console
       console.log(`Started server on port ${PORT}`);
     });
