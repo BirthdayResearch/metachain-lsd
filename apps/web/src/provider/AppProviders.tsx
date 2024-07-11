32cc7d19f57c2b1f148eba310c6ac8983c7e036e
chloe <44501120+chloezxyy@users.noreply.github.com>
2024-07-11 14:29:57 +0800

    refactor: remove mount

--- apps/web/src/provider/AppProviders.tsx
+++ apps/web/src/provider/AppProviders.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import { WagmiProvider } from "wagmi";
 import config from "@/config/wagmiConfig";
@@ -16,20 +16,13 @@ export default function AppProviders({
 }) {
   const queryClient = new QueryClient();
 
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <ConnectKitProvider options={{ initialChainId: 0 }}>
-          {mounted && (
-            <NetworkEnvironmentProvider>
-              <ContractProvider>{children}</ContractProvider>
-            </NetworkEnvironmentProvider>
-          )}
+          <NetworkEnvironmentProvider>
+            <ContractProvider>{children}</ContractProvider>
+          </NetworkEnvironmentProvider>
         </ConnectKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
