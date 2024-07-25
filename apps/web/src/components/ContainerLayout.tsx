f7f3399ffa5eaaaefd6a8d9b811246ac9f7233f9
chloe <44501120+chloezxyy@users.noreply.github.com>
2024-07-25 14:10:46 +0800

    fix: wrap providers suspense to fix build issues

--- apps/web/src/components/ContainerLayout.tsx
+++ apps/web/src/components/ContainerLayout.tsx
@@ -7,6 +7,7 @@ import React, { useRef, useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
 import AppProviders from "@/provider/AppProviders";
+import { Suspense } from "react";
 
 const inter = Montserrat({ subsets: ["latin"] });
 
@@ -24,30 +25,32 @@ export default function ContainerLayout({
   }, []);
 
   return (
-    <AppProviders>
-      <html lang="en">
-        <body
-          suppressHydrationWarning={true}
-          className={`${inter.className} absolute top-0 left-0 z-auto h-full w-full bg-cover bg-no-repeat bg-clip-border bg-[url('/background-mobile-375.svg')] md:bg-[url('/background-web-1440.svg')]`}
-        >
-          <Provider store={store}>
-            {mounted && (
-              <div
-                ref={contentRef}
-                className="flex min-h-screen flex-col items-center w-full text-light-1000"
-              >
-                {children}
-                <Next13ProgressBar
-                  height="4px"
-                  color="#69FF23"
-                  options={{ showSpinner: true }}
-                  showOnShallow
-                />
-              </div>
-            )}
-          </Provider>
-        </body>
-      </html>
-    </AppProviders>
+    <html lang="en">
+      <body
+        suppressHydrationWarning={true}
+        className={`${inter.className} absolute top-0 left-0 z-auto h-full w-full bg-cover bg-no-repeat bg-clip-border bg-[url('/background-mobile-375.svg')] md:bg-[url('/background-web-1440.svg')]`}
+      >
+        <Suspense>
+          <AppProviders>
+            <Provider store={store}>
+              {mounted && (
+                <div
+                  ref={contentRef}
+                  className="flex min-h-screen flex-col items-center w-full text-light-1000"
+                >
+                  {children}
+                  <Next13ProgressBar
+                    height="4px"
+                    color="#69FF23"
+                    options={{ showSpinner: true }}
+                    showOnShallow
+                  />
+                </div>
+              )}
+            </Provider>
+          </AppProviders>
+        </Suspense>
+      </body>
+    </html>
   );
 }
